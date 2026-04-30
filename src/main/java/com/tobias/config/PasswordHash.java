74bc6e18847f32ba1b91d1deb3ea54531a97caef
arthur-Gmp <arthurprates333@gmail.com>
2026-04-30 15:22:29 -0300

    feat: implementação do hash (#13)

---
+++ src/main/java/com/tobias/config/PasswordHash.java
@@ -0,0 +1,16 @@
+package com.tobias.config;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordHash {
+
+    // Gera o hash da senha
+    public static String hashPassword(String senha) {
+        return BCrypt.hashpw(senha, BCrypt.gensalt());
+    }
+
+    // Verifica se a senha digitada bate com o hash
+    public static boolean checkPassword(String senhaDigitada, String hashSalvo) {
+        return BCrypt.checkpw(senhaDigitada, hashSalvo);
+    }
+}
