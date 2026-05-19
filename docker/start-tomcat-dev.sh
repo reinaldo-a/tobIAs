f2f563ac025222fd509d863025514e425cd2ad9a
Reinaldo Junior <60711391+reinaldo-a@users.noreply.github.com>
2026-05-19 20:29:16 -0300

    Coreção versão migrations (#40)

--- docker/start-tomcat-dev.sh
+++ docker/start-tomcat-dev.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ -f /workspace/pom.xml ]; then
   echo "Compilando classes Java em /workspace..."
+  rm -rf /workspace/target/classes
   mvn -f /workspace/pom.xml compile
   rm -rf /usr/local/tomcat/webapps/ROOT/WEB-INF/classes
   cp -R /workspace/target/classes /usr/local/tomcat/webapps/ROOT/WEB-INF/classes
